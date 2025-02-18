3660624f50dd4b37a984b594d409e0354f0d77b9
vlr <valeriia.dccclxiv@gmail.com>
2025-02-18 21:34:50 +0000

    added simple Bayesian logistic regression model

--- analysis.R
+++ analysis.R
@@ -1,8 +1,21 @@
 library(dplyr)
+library(brms)
 
-data(mtcars)
-mtcars_summary <- mtcars %>%
-  select(mpg, cyl, disp) %>%
-  summarise_all(mean)
+data <- read.csv("jay_data.csv")
 
-print(mtcars_summary)
+data$response <- as.integer(data$response)
+
+model <- brm(
+  response ~ trees + roads + grass,
+  family = bernoulli(link = "logit"),
+  data = data,
+  prior = c(
+    set_prior("normal(0, 5)", class = "b"),   
+    set_prior("normal(0, 5)", class = "Intercept")
+  ),
+  chains = 4,
+  iter = 2000,
+  warmup = 1000
+)
+
+summary(model)
