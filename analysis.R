686ba677c3066c121c529750821d20b690566f64
vlr <valeriia.dccclxiv@gmail.com>
2025-03-02 13:25:28 +0200

    added brm model

--- analysis.R
+++ analysis.R
@@ -1,14 +1,32 @@
 library(dplyr)
+library(tidyr)
 library(brms)
+library(here)
 
-data <- read.csv("jay_data.csv")
+data_long <- read.csv(here("data", "jay_data.csv"), stringsAsFactors = FALSE)
 
-data$response <- as.integer(data$response)
+str(data_long)
+
+data_long <- data_long %>%
+  mutate(nest_bin = ifelse(nest_type == "Jay", 1, 0)) %>%
+  group_by(nest_type, category) %>%
+  mutate(nest_id = row_number()) %>%
+  ungroup()
+
+
+data_wide <- data_long %>%
+  pivot_wider(
+    id_cols = c(nest_type, nest_bin, nest_id),
+    names_from = category,
+    values_from = percentage
+  )
+
+str(data_wide)
 
 model <- brm(
-  response ~ trees + roads + grass,
+  nest_bin ~ percentage * category,
   family = bernoulli(link = "logit"),
-  data = data,
+  data = data_wide,
   prior = c(
     set_prior("normal(0, 5)", class = "b"),   
     set_prior("normal(0, 5)", class = "Intercept")
